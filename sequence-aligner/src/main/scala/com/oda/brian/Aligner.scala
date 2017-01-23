f6afa101304830f743e9f455bd790b72d6d5dd11
Brian Hill <brian.l.hill@icloud.com>
2017-01-23 13:18:10 -0800

    Fixed few things

--- sequence-aligner/src/main/scala/com/oda/brian/Aligner.scala
+++ sequence-aligner/src/main/scala/com/oda/brian/Aligner.scala
@@ -6,30 +6,31 @@ package com.oda.brian
 object Aligner {
   def main(args: Array[String]): Unit = {
     // read in reference lines from file
-    val fileLines = io.Source.fromFile("ref_practice_W_1_chr_1.txt").getLines.toList
+    val fileLines = io.Source.fromFile("/nfs/home/blhill/code/github/SequenceAligner/sequence-aligner/src/test/resources/practice_W_1/ref_practice_W_1_chr_1.txt").getLines.toList
     // turn lines into a single string, skipping header line
-    val reference_string = fileLines.slice(1, fileLines.length)
+    val reference_string = fileLines.slice(1, fileLines.length).mkString
 
     val test_string = new StringBuilder
-    test_string ++= "ACAACG"
+    //test_string ++= "ACAACG"
+    test_string ++= reference_string
     val BWT = BWTMatrix(test_string)
     BWT.foreach(println)
     getBWTMatrixFirstLastCols(BWT).foreach(println)
-    
+   
   }
 
   def BWTMatrix(T: StringBuilder) : Array[String] = {
     // create empty array of strings
     val arr_buff = collection.mutable.ArrayBuffer.empty[String]
     // if last character isn't a $, append one
-    if (T(T.length - 1) != "$") {
+    if (T(T.length - 1) != '$') {
       T ++= "$"
     }
     // get length of string
     val len = T.length
     // rotate the sequence by slicing at a location and appending it
     for (i <- 0 until len) {
-      val s = T.slice(i, len) + T.slice(0, i)
+      val s = T.toString.slice(i, len) + T.toString.slice(0, i)
       arr_buff += s
     }
     // sort the string and return
