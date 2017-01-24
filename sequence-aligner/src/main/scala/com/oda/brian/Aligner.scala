a861e245916241b377666c0f96cb57d253ab2658
Brian Hill <brian.l.hill11@gmail.com>
2017-01-23 22:42:04 -0800

    added first spark code

--- sequence-aligner/src/main/scala/com/oda/brian/Aligner.scala
+++ sequence-aligner/src/main/scala/com/oda/brian/Aligner.scala
@@ -59,10 +59,12 @@ object Aligner {
       count_arr += last_col.slice(0, i).count(c => c == last_col(i))
     }
 
-    reads.foreach(x => println(getMatchRange(x, last_col, count_arr, char_offset_map)))
+    //reads.foreach(x => println(getMatchRange(x, last_col, count_arr, char_offset_map)))
+    val match_ranges = distReads.map(x => getMatchRange(x, last_col, count_arr, char_offset_map))
 
-    val match_range = getMatchRange("ACG", first_last_cols._2, count_arr, char_offset_map)
-    getSequencePosition(last_col, count_arr, char_offset_map, match_range)
+    //val match_range = getMatchRange("ACG", first_last_cols._2, count_arr, char_offset_map)
+    val seq_positions = match_ranges.map(x => getSequencePosition(last_col, count_arr, char_offset_map, x))
+    seq_positions.collect.foreach(println)
   }
 
   def BWTMatrix(T: StringBuilder) : Array[String] = {
