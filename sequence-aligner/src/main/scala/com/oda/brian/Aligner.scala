175300159451802aafc4b8f38e016abf75dac09c
Brian Hill <brian.l.hill11@gmail.com>
2017-01-24 16:31:00 -0800

    Refactored code

--- sequence-aligner/src/main/scala/com/oda/brian/Aligner.scala
+++ sequence-aligner/src/main/scala/com/oda/brian/Aligner.scala
@@ -4,13 +4,14 @@ import util.control.Breaks._
 import scala.collection.mutable.ArrayBuffer
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-
+import java.io.{BufferedWriter, File, FileWriter}
 /**
   * Created by blhill on 1/22/17.
   */
 object Aligner {
   def main(args: Array[String]): Unit = {
 
+    val runTest = 1
     val conf = new SparkConf().setAppName("Aligner")
     val sc = new SparkContext(conf)
 
@@ -26,49 +27,70 @@ object Aligner {
     // parallelize the reads in spark
     val distReads = sc.parallelize(reads)
     //
-    //
+
+    if (runTest == 1) {
+      val test_string = new StringBuilder
+      test_string ++= "ACAACG"
+
+      val BWTMatrix_test = BWTMatrix(test_string)
+      val BWT_test = getBWTMatrixLastCol(BWTMatrix_test)
+      val countMap_test = createCountMap(BWT_test)
+      val occurrences_test = createOccurrences(BWT_test)
+      val test_queries = List("ACC", "CAA", "ACG", "GAC")
+      test_queries.foreach(x => println(EXACTMATCH(x, countMap_test, occurrences_test)))
+      System.exit(0)
+    }
     val test_string = new StringBuilder
     //test_string ++= "ACAACG"
     test_string ++= reference_string
-    val BWT = BWTMatrix(test_string)
-    //BWT.foreach(println)
-    //getBWTMatrixFirstLastCols(BWT).foreach(println)
-    val first_last_cols = getBWTMatrixFirstLastCols(BWT)
 
-    val last_col = first_last_cols._2
-    // CREATE MAP BETWEEN CHAR AND OFFSET
-    // get counts of letters
-    val num_A = last_col.count(c => c == 'A')
-    val num_C = last_col.count(c => c == 'C')
-    val num_G = last_col.count(c => c == 'G')
-    val num_T = last_col.count(c => c == 'T')
-    // use counts of letters to get offsets (1 for the $ char)
-    val offset_A = 1
-    val offset_C = offset_A + num_A
-    val offset_G = offset_C + num_C
-    val offset_T = offset_G + num_G
-
-    // create map between character and offset
-    val char_offset_map = Map('A' -> offset_A, 'C' -> offset_C, 'G' -> offset_G, 'T' -> offset_T, '$' -> 0)
-    println("charoffsetmap: " + char_offset_map)
-
-    // get array consisting of counts of that letter up until that location
-    val count_arr = ArrayBuffer.empty[Int]
-
-    for (i <- 0 until last_col.length) {
-      // count number of instances of letter at index in last col up until that point
-      count_arr += last_col.slice(0, i).count(c => c == last_col(i))
+    var BWT = ""
+    // if we don't get a .bwt file as input, create the transform
+    if (args.length < 3) {
+      val BWTMatrix = BWTMatrix(test_string)
+      BWT = getBWTMatrixLastCol(BWTMatrix)
+      writeBWT(BWT, args(0) + ".bwt")
     }
+    // otherwise read from file
+    else {
+      BWT = readBWT(args(2))
+    }
+
+    //BWT.foreach(println)
+
+    var countMap: Map[Char, Int] = Map()
+    // if we don't get a .cntmap file as input, create the count map
+    if (args.length < 4) {
+      countMap = createCountMap(BWT)
+      writeCountMap(countMap, args(0) + ".cntmap")
+    }
+    // otherwise read from file
+    else {
+      countMap = readCountMap(args(3))
+    }
+
+    var occurrences = List.empty[Int]
+    // if we don't get a .occ file as input, create the occurrences list
+    if (args.length < 5) {
+      occurrences = createOccurrences(BWT)
+      writeOccurrences(occurrences, args(0) + ".occ")
+    }
+    // else read from file
+    else {
+      occurrences = readOccurrences(args(4))
+    }
+
+
 
     println("getting match ranges...")
     //reads.foreach(x => println(getMatchRange(x, last_col, count_arr, char_offset_map)))
-    val match_ranges = distReads.map(x => getMatchRange(x.slice(0, 6), last_col, count_arr, char_offset_map))
+    //val match_ranges = distReads.map(x => getMatchRange(x.slice(0, 6), last_col, count_arr, char_offset_map))
 
     println("getting sequence positions...")
     //val match_range = getMatchRange("ACG", first_last_cols._2, count_arr, char_offset_map)
-    val seq_positions = match_ranges.map(x => getSequencePosition(last_col, count_arr, char_offset_map, x))
-    seq_positions.collect.foreach(println)
-    println("Number of matched reads: " + seq_positions.collect.count(x => x > 0))
+    //val seq_positions = match_ranges.map(x => getSequencePosition(last_col, count_arr, char_offset_map, x))
+    //seq_positions.collect.foreach(println)
+    //println("Number of matched reads: " + seq_positions.collect.count(x => x > 0))
   }
 
   def BWTMatrix(T: StringBuilder) : Array[String] = {
@@ -89,11 +111,103 @@ object Aligner {
     arr_buff.toArray.sorted
   }
 
-  def getBWTMatrixFirstLastCols(BWTMatrix: Array[String]) : (List[Char], List[Char]) = {
+  def getBWTMatrixLastCol(BWTMatrix: Array[String]) : String = {
     // return a tuple of the first character and the last character of each string
-    val first_col = BWTMatrix.map(s => s(0))
-    val last_col = BWTMatrix.map(s => s(s.length - 1))
-    return (first_col.toList, last_col.toList)
+    //val first_col = BWTMatrix.map(s => s(0))
+    val last_col_chars = BWTMatrix.map(s => s(s.length - 1))
+    val last_col_string = new StringBuilder
+    for (i <- last_col_chars) {
+      last_col_string ++= i.toString
+    }
+    return last_col_string.toString
+  }
+
+  def writeBWT(BWT: String, Filename: String): Unit = {
+    val file = new File(Filename + ".bwt")
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(BWT)
+    bw.close
+  }
+
+  def readBWT(Filename: String) : String = {
+    val BWT = scala.io.Source.fromFile(Filename).getLines.mkString
+    return BWT
+  }
+
+  def createCountMap(BWT: String) : Map[Char, Int] = {
+    // CREATE MAP BETWEEN CHAR AND OFFSET
+    // get counts of letters
+    val num_A = BWT.count(c => c == 'A')
+    val num_C = BWT.count(c => c == 'C')
+    val num_G = BWT.count(c => c == 'G')
+    val num_T = BWT.count(c => c == 'T')
+    // use counts of letters to get offsets (1 for the $ char)
+    val offset_A = 0
+    val offset_C = offset_A + num_A
+    val offset_G = offset_C + num_C
+    val offset_T = offset_G + num_G
+
+    // create map between character and offset
+    val char_count_map = Map('A' -> offset_A, 'C' -> offset_C, 'G' -> offset_G, 'T' -> offset_T, '$' -> 0)
+    println("charoffsetmap: " + char_count_map)
+    return char_count_map
+  }
+
+  def writeCountMap(countMap: Map[Char, Int], Filename: String): Unit = {
+    val file = new File(Filename + ".cntmap")
+    val bw = new BufferedWriter(new FileWriter(file))
+    val map_strings = countMap.map(x => x._1 + "," + x._2)
+    for (line <- map_strings) {
+      bw.write(line + "\n")
+    }
+    bw.close
+  }
+
+  def readCountMap(Filename: String) : Map[Char, Int] = {
+    val countMapPairs = scala.io.Source.fromFile(Filename).getLines.map(_.split(","))
+    var countMap: Map[Char, Int] = Map()
+    countMapPairs.foreach(x => countMap += (x(0)(0) -> x(1).toInt))
+    return countMap
+  }
+
+  def createOccurrences(BWT: String): List[Int] = {
+    // get array consisting of counts of that letter up until that location
+    val occurrences = ArrayBuffer.empty[Int]
+
+    for (i <- 0 until BWT.length) {
+      // count number of instances of letter at index in last col up until that point
+      occurrences += BWT.slice(0, i).count(c => c == BWT(i))
+    }
+    return occurrences.toList
+  }
+
+  def writeOccurrences(Occurrences: List[Int], Filename: String): Unit = {
+    val file = new File(Filename + ".occ")
+    val bw = new BufferedWriter(new FileWriter(file))
+    for (line <- Occurrences) {
+      bw.write(line + "\n")
+    }
+    bw.close
+  }
+
+  def readOccurrences(Filename: String) : List[Int] = {
+    val occurrences = scala.io.Source.fromFile(Filename).getLines.toList.map(_.toInt)
+    return occurrences
+  }
+
+  def EXACTMATCH(query: String, countMap: Map[Char, Int], occurrence: List[Int]): (Int, Int) = {
+    var c = query(query.length - 1)
+    var start_ptr = countMap(c) + 1
+    var end_ptr = countMap(c + 1) + 1
+    var i = query.length - 2
+
+    while (start_ptr < end_ptr && i >= 1) {
+      c = query(i)
+      start_ptr = countMap(c) + occurrence(start_ptr) + 1
+      end_ptr = countMap(c) + occurrence(end_ptr) + 1
+      i = i - 1
+    }
+    return (start_ptr, end_ptr)
   }
 
   def getMatchRange(query: String, last_col: List[Char], count_arr: ArrayBuffer[Int], char_offset_map: Map[Char, Int] ) : (Int, Int) = {
